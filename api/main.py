922d758e9f8a4682324b46fcf5a21c5c6ed2c8d3
oshiro <kaito.o.0628@gmail.com>
2024-02-18 21:34:44 +0900

    ルーターの設定を追加

--- api/main.py
+++ api/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from api.routers import task, done
 
 app = FastAPI()
-
-# デコレータ
-@app.get("/hello")
-async def hello():
-  return {"message": "hello world!"}
+app.include_router(task.router)
+app.include_router(done.router)
